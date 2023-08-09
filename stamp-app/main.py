dcfcde9eea6538c678cf7863f62f2e06208def70
Paul Cohen <paul.cohen@seibostudios.se>
2023-08-09 17:48:24 +0200

    Refactor code for displaying search results

--- stamp-app/main.py
+++ stamp-app/main.py
@@ -9,7 +9,7 @@ import httpx
 import datetime
 import time
 import hashlib
-import search_navigation
+import search
 
 
 class Settings(BaseSettings):
@@ -22,7 +22,7 @@ class Settings(BaseSettings):
     TEMPLATES_DIR: str = "templates"
     DATE_FORMAT: str = "Date: %a, %d %b %Y %H:%M:%S"
     CATALOGUE_API_URL: str = "http://127.0.0.1:8081/"
-    RESULTS_PER_PAGE: int = 20
+    RESULTS_PER_PAGE: int = 5
 
     class ConfigDict:
         env_file = ".env"
@@ -73,32 +73,27 @@ async def get_index_file(request: Request) -> HTMLResponse:
 async def get_search(request: Request,
                      q: Optional[str] = Query(None,
                                               description="Search query"),
-                     start: Optional[int] = Query(None,
+                     start: Optional[int] = Query(default=0,
                                                   description="Start result")) -> HTMLResponse:
     """The search page (search.hmtl)."""
     tic = time.perf_counter_ns()
     if q:
         url = f"{settings.CATALOGUE_API_URL}stamps?title={q}"
-        print(f"!!url: {url}")
         r = httpx.get(url)
         if r.status_code == 200:
-            results = r.json()
-            seconds = float(r.headers["server-timing"].split("=")[1])/1000
-            if not start:
-                start = 0
-            rps = search_navigation.result_pages_specification(results["count"],
-                                                               start,
-                                                               settings.RESULTS_PER_PAGE,
-                                                               linked_pages=10,
-                                                               first_page=True,
-                                                               last_page=True)
-            stamps = results["stamps"][start:start+settings.RESULTS_PER_PAGE]
+            ssr = search.stamp_search_results(r,
+                                              start,
+                                              settings.RESULTS_PER_PAGE)
+            rps = search.search_result_page_spec(ssr["stamps_count"],
+                                                 start,
+                                                 settings.RESULTS_PER_PAGE,
+                                                 linked_pages=10,
+                                                 first_page=True,
+                                                 last_page=True)
             result = templates.TemplateResponse("search.html",
                                                 {"request": request,
-                                                 "count": results["count"],
-                                                 "rps": rps,
-                                                 "stamps": stamps,
-                                                 "seconds": f"{seconds:.3}"})
+                                                 "ssr": ssr,
+                                                 "rps": rps})
             result.headers["HX-Push"] = f"/search?q={q}"
     else:
         rps = None
@@ -113,7 +108,7 @@ async def get_search(request: Request,
 async def get_search_results(request: Request, response: Response,
                              q: Optional[str] = Query(None,
                                                       description="Search query"),
-                             start: Optional[int] = Query(None,
+                             start: Optional[int] = Query(default=0,
                                                           description="Start")) -> HTMLResponse:
     """HTML representation of the search results (search_results.html)."""
     tic = time.perf_counter_ns()
@@ -122,36 +117,26 @@ async def get_search_results(request: Request, response: Response,
         url = f"{settings.CATALOGUE_API_URL}stamps?title={q}"
     else:
         url = f"{settings.CATALOGUE_API_URL}stamps"
-    print(f"url: {url}")
     r = httpx.get(url)
     # Set up the HTMX-response
     if r.status_code == 200:
-        results = r.json()
-        # Generate HTML search result with appropriate Jinja2template
-        seconds = float(r.headers["server-timing"].split("=")[1])/1000
-        if not start:
-            start = 0
-        rps = search_navigation.result_pages_specification(results["count"],
-                                                           start,
-                                                           settings.RESULTS_PER_PAGE,
-                                                           linked_pages=10,
-                                                           first_page=True,
-                                                           last_page=True)
-        stamps = results["stamps"][start:start+settings.RESULTS_PER_PAGE]
-        if q == "Empereur Napoleon III":
-            print(stamps)
+        ssr = search.stamp_search_results(r,
+                                          start,
+                                          settings.RESULTS_PER_PAGE)
+        rps = search.search_result_page_spec(ssr["stamps_count"],
+                                             start,
+                                             settings.RESULTS_PER_PAGE,
+                                             linked_pages=10,
+                                             first_page=True,
+                                             last_page=True)
         result = templates.TemplateResponse("search_results.html",
                                             {"request": request,
-                                             "count": results["count"],
-                                             "rps": rps,
-                                             "stamps": stamps,
-                                             "seconds": f"{seconds:.3}"})
+                                             "ssr": ssr,
+                                             "rps": rps})
         result.headers["HX-Push"] = f"/search?q={q}"
         toc = time.perf_counter_ns()
         # Set Server-timing header (server excution time in ms, not including FastAPI itself)
         result.headers["Server-timing"] = f"API;dur={(toc - tic)/1000000}"
-        if q == "Empereur Napoleon III":
-            print(result.body)
         return result
     else:
         response.status_code = status.HTTP_404_NOT_FOUND
@@ -167,7 +152,6 @@ async def get_stamp_variants(request: Request, response: Response,
     """HTML representation of a stamps variants."""
     tic = time.perf_counter_ns()
     url = f"{settings.CATALOGUE_API_URL}stamps/{stamp_id}"
-    print(f"url: {url}")
     r = httpx.get(url)
     if r.status_code == 200:
         variants = r.json()["variants"]
@@ -191,7 +175,6 @@ async def get_stamp_image(response: Response,
                           stamp_id: Annotated[str, Path(title="Id of stamp")]) -> Response:
     tic = time.perf_counter_ns()
     url = f"{settings.CATALOGUE_API_URL}stamps/{stamp_id}/image"
-    print(f"url: {url}")
     r = httpx.get(url)
     if r.status_code == 200:
         image = r.content
---
+++ stamp-app/search.py
@@ -0,0 +1,130 @@
+# This is a utility module for with features for preproccessing search results from the Faststamps
+# catalogue-api before the search results are rendered in HTML.
+
+from typing import List
+from pydantic import BaseModel
+# import pytest
+
+
+class StampSearchResults(BaseModel):
+    """The results of a stamp search with some calculated attributes with information useful for
+       rendering the HTML that represents the search results."""
+    count: int
+    stamps_count: int
+    variant_stamps_count: int
+    stamps: List[dict]
+    seconds: str
+
+
+class SearchResultPageSpec(BaseModel):
+    """A specification of how to construct a search results page with links, given a number of
+       search results, a given number of "results per page", a current page of results to present
+       based on a `start`search result, and finally a specification of other navigational links
+       to other pages with search results."""
+    page_count: int
+    current_page: int
+    linked_pages: List[int]
+    next_page: bool
+    previous_page: bool
+    first_page: bool
+    last_page: bool
+    left_ellipsis: bool
+    right_ellipsis: bool
+
+
+def search_result_page_spec(count: int,
+                            start: int,
+                            rpp: int,
+                            linked_pages: int,
+                            first_page: bool = False,
+                            last_page: bool = False,
+                            previous_page: bool = False,
+                            next_page: bool = False) -> SearchResultPageSpec:
+    """Specification of how to construct a search results page given `count` (number of items /
+       search results), `start` (the first item / search result to  display) and `rpp` (the
+       number of items / results per page to display)."""
+    assert count >= 0
+    assert start >= 0
+    assert rpp > 0
+    assert start % rpp == 0
+    assert linked_pages >= 0
+
+    # Calculate total number of pages
+    page_count = count // rpp + 1
+    # Calculate the current page number
+    current_page = start // rpp + 1
+
+    result = {"page_count": page_count,
+              "current_page": current_page,
+              "linked_pages": [],
+              "next_page": next_page,
+              "previous_page": previous_page,
+              "first_page": first_page,
+              "last_page": last_page}
+
+    if linked_pages >= page_count:
+        # All pages will have links
+        first = 1
+        last = page_count
+    elif linked_pages == 0:
+        first = 0
+        last = -1
+    elif linked_pages == 1:
+        first = current_page
+        last = current_page
+    elif linked_pages > 1:
+        # Calculate number of pages left and right of current page `n` that will have links. In the
+        # case of an even number of linked pages, we link one more page to the left than to the
+        # right.
+        first = current_page - linked_pages // 2
+        last = current_page + linked_pages // 2
+        if linked_pages % 2 == 0:
+            last -= 1
+        # If we've overflowed to the left or right we need to adjust left and right
+        if first <= 0:
+            last = last + 1 - first
+            first = 1
+        if last > page_count:
+            first = first - (last - page_count)
+            last = page_count
+
+    # Build representation with links
+    for i in range(first, last+1):
+        result["linked_pages"].append(i)
+
+    # Check if first page, last page and ellipsis should be shown
+    if 1 in result["linked_pages"]:
+        result["first_page"] = False
+    else:
+        result["first_page"] = True
+    if page_count in result["linked_pages"]:
+        result["last_page"] = False
+    else:
+        result["last_page"] = True
+    if result["linked_pages"][0] > 1:
+        result["left_ellipsis"] = True
+    else:
+        result["left_ellipsis"] = False
+    if result["linked_pages"][-1] < page_count:
+        result["right_ellipsis"] = True
+    else:
+        result["right_ellipsis"] = False
+
+    return result
+
+
+def stamp_search_results(response, start, results_per_page) -> StampSearchResults:
+    """A StampSearchResults object based on the response from a call to the stamp-catalogue
+       API, suitable for passing to the HTML rendering function."""
+    search_time = float(response.headers["server-timing"].split("=")[1])/1000
+    results = response.json()
+    # results["stamps"] contains a mixed list of stamps AND stamp variants, so we filter out the
+    # proper stamps.
+    stamps = [stamp for stamp in results["stamps"] if stamp["id"]["yt_variant"] == '']
+    stamps_count = len(stamps)
+    result = {"count": results["count"],
+              "stamps_count": stamps_count,
+              "variant_stamps_count": results["count"] - stamps_count,
+              "stamps": stamps[start:start + results_per_page],
+              "search_time": f"{search_time:.3}"}
+    return result
